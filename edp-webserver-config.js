673068ad54dd1f561f5170cb801c4a29d9664635
Yao Chang <yaochang@baidu.com>
2015-07-22 15:41:00 +0800

    first commit

---
+++ edp-webserver-config.js
@@ -0,0 +1,78 @@
+exports.port = 8848;
+exports.directoryIndexes = true;
+exports.documentRoot = __dirname;
+exports.getLocations = function () {
+    return [
+        {
+            location: /\/$/,
+            handler: home( 'index.html' )
+        },
+        {
+            location: function (req) {
+                console.log(req.pathname);
+                var pathname = req.pathname;
+                var paths = [
+                    '/bdbox/hot'
+                ];
+                for (var i = 0, len = paths.length; i < len; i++) {
+                    if (new RegExp(paths[i]).test(pathname)) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+            handler: [
+                proxy('cp01-rdqa04-dev152.cp01.baidu.com', 8092)
+            ]
+        },
+        {
+            location: /^\/redirect-local/,
+            handler: redirect('redirect-target', false)
+        },
+        {
+            location: /^\/redirect-remote/,
+            handler: redirect('http://www.baidu.com', false)
+        },
+        {
+            location: /^\/redirect-target/,
+            handler: content('redirectd!')
+        },
+        {
+            location: '/empty',
+            handler: empty()
+        },
+        {
+            location: /\.css($|\?)/,
+            handler: [
+                autocss()
+            ]
+        },
+        {
+            location: /\.less($|\?)/,
+            handler: [
+                file(),
+                less()
+            ]
+        },
+        {
+            location: /\.styl($|\?)/,
+            handler: [
+                file(),
+                stylus()
+            ]
+        },
+        {
+            location: /^.*$/,
+            handler: [
+                file(),
+                proxyNoneExists()
+            ]
+        }
+    ];
+};
+
+exports.injectResource = function ( res ) {
+    for ( var key in res ) {
+        global[ key ] = res[ key ];
+    }
+};
